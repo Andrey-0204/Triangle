7d304fe89629faca774ca0768826c19f2e641e57
Andrey-0204 <109725281+Andrey-0204@users.noreply.github.com>
2022-07-23 21:45:37 +0300

    Начало работы треугольник

--- 6/Program.cs
+++ 6/Program.cs
@@ -11,6 +11,19 @@ namespace _6
         static void Main(string[] args)
         {
 
+            for (int i = 0; i < 10; i++)       //треугольник
+            {
+
+                for (int j = 0; j < 5; j++)
+                {
+                    Console.Write("*");
+                }
+
+
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("\n");
         }
     }
 }
