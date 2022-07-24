92bf891d4c4b2a1f9028b05a151faebaf5547a07
Andrey-0204 <109725281+Andrey-0204@users.noreply.github.com>
2022-07-24 12:57:02 +0300

    Равнобедренный треугольник

--- 6/Program.cs
+++ 6/Program.cs
@@ -10,7 +10,7 @@ namespace _6
     {
         static void DrawRectangle(int heigth, int width)
         {
-            for (int i = 0; i < heigth; i++)       //треугольник
+            for (int i = 0; i < heigth; i++)       //прямоугольник
             {
 
                 for (int j = 0; j < width; j++)
@@ -22,9 +22,32 @@ namespace _6
                 Console.WriteLine();
             }
         }
+        static void DrawTriangle(int width)
+        {
+            int razmer = 5;
+            for (int i = 0; i < width; i++)       //треугольник
+            {
+
+                for (int j = width; j > 0; j--)
+                {
+                    if (i >= j) Console.Write("[]");
+                    else Console.Write("  ");
+                }
+                for (int j = 0; j < width; j++)
+                {
+                    if (i >= j) Console.Write("[]");
+                    else Console.Write("  ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+
         static void Main(string[] args)
         {
-            DrawRectangle(4, 15);
+            DrawTriangle(10);
+            //DrawRectangle(5, 5);
             Console.ReadLine();
         }
     }
